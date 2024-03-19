f87c3e279e8554f0cbd0f55392963e7e73ed9995
Lebohang Sedoaba <Lebohang@adoozy.co.za>
2024-03-19 04:23:10 +0200

    check if word is valid check if the dictionary has the sentance already

--- TS.TechnicalTest/QuestionOne.cs
+++ TS.TechnicalTest/QuestionOne.cs
@@ -20,7 +20,23 @@ public class QuestionOne
         {
             //so for each sentance i will split into words when i find a space in the sentance
             words = sentance.Split(new char[] { ' ' });
-            Console.WriteLine(words);
+
+            foreach(string word in words)
+            {
+                //check if the word is valid by check its length
+                if(word.Length > 0)
+                {
+                    //when the word is valid i do a check if my dictionary has the sentance stored
+                    if (sentanceAndWordCount.ContainsKey(sentance))
+                    {
+                        Console.WriteLine("Found");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Must Store");
+                    }
+                }
+            }
         }
 
         return highestWordCount;
