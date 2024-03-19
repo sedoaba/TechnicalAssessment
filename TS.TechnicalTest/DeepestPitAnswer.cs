3935c78a9d5fe8ecd38836a671bd4209c7837478
Lebohang Sedoaba <Lebohang@adoozy.co.za>
2024-03-19 03:56:10 +0200

    init

---
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace TS.TechnicalTest;
+public class DeepestPitAnswer
+{
+    public static int Solution(int[] points)
+    {
+        throw new NotImplementedException("Not completed yet");
+    }
+}
